7a4ea62b217d9fc6142043b8141839cca0f4c966
Siviwe Ngwilingwili <YOUR_GITHUB_EMAIL@example.com>
2025-12-17 13:49:06 +0200

    Initial task submission - SN25110019295

---
+++ my_variables.py
@@ -0,0 +1,16 @@
+# Student Name: Siviwe Ngwilingwili
+# Student Number: SN25110019295
+
+name = "Siviwe"
+age = 47
+height = 1.75
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+age_str = str(age)
+height_int = int(height)
+
+print("age_str type:", type(age_str), age_str)
+print("height_int:", height_int)
